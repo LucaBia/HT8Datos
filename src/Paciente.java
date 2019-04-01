bb35a845f27c75a10e3abcabed5d9232901b3e0a
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-04-01 17:22:54 -0600

    Avances

---
+++ src/Paciente.java
@@ -0,0 +1,16 @@
+public class Paciente implements Comparable {
+    String nombre;
+    String sintoma;
+    String codigo; //A a E
+
+    Paciente (String nombre, String sintoma, String codigo) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigo = codigo;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
