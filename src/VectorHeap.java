ad1a74efab6b8cc7bec69cef7b91edb2cfc40e8f
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-04-04 19:16:41 -0600

    Completacion

--- src/VectorHeap.java
+++ src/VectorHeap.java
@@ -1,15 +1,28 @@
 import java.util.Vector;
 
+
+/**
+ * Clase que define la prioridad de los pacientes.
+ * La clase implemente PriorityQueue.
+ * @param <E>
+ */
 public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 
 	protected Vector<E> data; // the data, kept in heap order
 
+	/**
+	 * Constructor
+	 */
 	public VectorHeap()
 	// post: constructs a new priority queue
 	{
 		data = new Vector<E>();
 	}
 
+	/**
+	 * El metodo construye un nuevo priority queue de un vector desordenado (sin prioridad)
+	 * @param v
+	 */
 	public VectorHeap(Vector<E> v)
 	// post: constructs a new priority queue from an unordered vector
 	{
@@ -20,13 +33,22 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 		}
 	}
 
+	/**
+	 * Retorna el padre de un nodo en la localidad i
+	 * @param i
+	 * @return padre del nodo
+	 */
 	protected static int parent(int i)
 	// pre: 0 <= i < size
-	// post: returns parent of node at location i
 	{
 		return (i - 1) / 2;
 	}
 
+	/**
+	 * Retorna el indice del hijo izquierdo de un nodo en la posicion i
+	 * @param i
+	 * @return el indice del hijo izquierdo
+	 */
 	protected static int left(int i)
 	// pre: 0 <= i < size
 	// post: returns index of left child of node at location i
@@ -34,16 +56,23 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 		return 2 * i + 1;
 	}
 
+	/**
+	 * Retorna el indice del hijo derecho de un nodo en la posicion i
+	 * @param i
+	 * @return el indice del hijo derecho
+	 */
 	protected static int right(int i)
 	// pre: 0 <= i < size
-	// post: returns index of right child of node at location i
 	{
 		return (2 * i + 1) + 1;
 	}
 
+	/**
+	 * Mueve el nodo en el indice de la hoja hasta la posicion apropiada
+	 * @param leaf
+	 */
 	protected void percolateUp(int leaf)
 	// pre: 0 <= leaf < size
-	// post: moves node at index leaf up to appropriate position
 	{
 		int parent = parent(leaf);
 		E value = data.get(leaf);
@@ -56,18 +85,22 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 		data.set(leaf, value);
 	}
 
+	/**
+	 * Agrega un valor a la cola de prioridad
+	 * @param value
+	 */
 	public void add(E value)
-	// pre: value is non-null comparable
-	// post: value is added to priority queue
 	{
 		data.add(value);
 		percolateUp(data.size() - 1);
 	}
 
+	/**
+	 * Mueve el nodo al indice de la raiz de abajo hacia la posicion apropiada en el subarbol
+	 * @param root
+	 */
 	protected void pushDownRoot(int root)
 	// pre: 0 <= root < size
-	// post: moves node at index root down
-	// to appropriate position in subtree
 	{
 		int heapSize = data.size();
 		E value = data.get(root);
@@ -95,9 +128,11 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 		}
 	}
 
+	/**
+	 * Elimina un valor (minimo) de la cola
+	 * @return valor minimo
+	 */
 	public E remove()
-	// pre: !isEmpty()
-	// post: returns and removes minimum value from queue
 	{
 		E minVal = getFirst();
 		data.set(0, data.get(data.size() - 1));
@@ -106,22 +141,38 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueueI<E> {
 		return minVal;
 	}
 
+	/**
+	 * Captura el primer valor del vector
+	 * @return primer valor
+	 */
 	public E getFirst() {
 		return data.get(0);
 	}
 
+	/**
+	 * Comprueba si el vector esta vacio
+	 * @return verdadero o falso
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size() == 0;
 	}
+
+	/**
+	 * Retorna el tamanio del vector
+	 * @return tamanio
+	 */
 
 	@Override
 	public int size() {
 		return data.size();
 	}
 
+	/**
+	 * Elimina todos los datos del vector
+	 */
 	@Override
 	public void clear() {
-
+		data.clear();
 	}
 }
